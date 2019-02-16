654bc9da95f0d54dc062b568c4869fc345585604
Adam Mytton <a.mytton@stratec.com>
2019-02-16 21:14:01 +0000

    Implement copyright XML generator.

---
+++ Present.CodeGeneration/Contracts/ICopyrightXmlGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace Present.CodeGeneration.Contracts
+{
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Contract for the copyright XML generator.
+    /// </summary>
+    public interface ICopyrightXmlGenerator
+    {
+        /// <summary>
+        /// Generates a Roslyn comment trivia for the XML copyright header.
+        /// </summary>
+        /// <returns>The generated XML copyright header.</returns>
+        DocumentationCommentTriviaSyntax Generate();
+    }
+}
