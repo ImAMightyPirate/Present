ae8bfc7729e538a2809bc74ecc660a9171bf14b6
Adam Mytton <adammytton@gmail.com>
2019-03-02 16:39:57 +0000

    Add new attribute constants for the MEF namespaces.

---
+++ Present.CodeGeneration/Constants/Attribute.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Present.NET. All Rights Reserved.
+// Present.NET is licensed under the MIT License. For usage and redistribution terms please refer to the LICENSE file.
+// For more information about the Present.NET project visit https://github.com/ImAMightyPirate/Present.
+
+namespace Present.CodeGeneration.Constants
+{
+    /// <summary>
+    /// Attribute constants.
+    /// </summary>
+    public static class Attribute
+    {
+        /// <summary>
+        /// The MEF export attribute.
+        /// </summary>
+        public const string MefExport = "System.ComponentModel.Composition.ExportAttribute";
+
+        /// <summary>
+        /// The MEF2 export attribute.
+        /// </summary>
+        public const string Mef2Export = "System.Composition.ExportAttribute";
+    }
+}
