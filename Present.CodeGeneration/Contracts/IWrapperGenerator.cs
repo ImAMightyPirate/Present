60e3f3adea0da61cb7eef1ad677a3ba021a0de42
Adam Mytton <adammytton@gmail.com>
2019-03-02 16:45:42 +0000

    Add wrapper generator class that acts as the glue between the invidiual code generators.

---
+++ Present.CodeGeneration/Contracts/IWrapperGenerator.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Present.NET. All Rights Reserved.
+// Present.NET is licensed under the MIT License. For usage and redistribution terms please refer to the LICENSE file.
+// For more information about the Present.NET project visit https://github.com/ImAMightyPirate/Present.
+
+namespace Present.CodeGeneration.Contracts
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Contract for the wrapper generator.
+    /// </summary>
+    public interface IWrapperGenerator
+    {
+        /// <summary>
+        /// Generates a Roslyn namespace definition from a type and its methods.
+        /// </summary>
+        /// <param name="options">The program options.</param>
+        /// <param name="typeNamespace">The namespace which the type belongs to.</param>
+        /// <param name="typeName">The name of the type.</param>
+        /// <param name="methods">The methods to be wrapped.</param>
+        /// <returns>The generated namespace declaration.</returns>
+        NamespaceDeclarationSyntax Generate(
+            IWrapOptions options,
+            string typeNamespace,
+            string typeName,
+            IEnumerable<MethodInfo> methods);
+    }
+}
---
+++ Present.CodeGeneration/Generators/WrapperGenerator.cs
@@ -0,0 +1,151 @@
+﻿// Copyright (c) Present.NET. All Rights Reserved.
+// Present.NET is licensed under the MIT License. For usage and redistribution terms please refer to the LICENSE file.
+// For more information about the Present.NET project visit https://github.com/ImAMightyPirate/Present.
+
+namespace Present.CodeGeneration.Generators
+{
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Reflection;
+    using EnsureThat;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Present.CodeGeneration.Constants;
+    using Present.CodeGeneration.Contracts;
+
+    /// <summary>
+    /// Responsible for generating the complete Royslyn definition for a wrapper.
+    /// </summary>
+    public class WrapperGenerator : IWrapperGenerator
+    {
+        private readonly INamespaceCodeGenerator namespaceCodeGenerator;
+        private readonly IAttributeCodeGenerator attributeCodeGenerator;
+        private readonly IInterfaceCodeGenerator interfaceCodeGenerator;
+        private readonly IClassCodeGenerator classCodeGenerator;
+        private readonly IMethodCodeGenerator methodCodeGenerator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WrapperGenerator"/> class.
+        /// </summary>
+        /// <param name="namespaceCodeGenerator">The namespace generator.</param>
+        /// <param name="attributeCodeGenerator">The attribute code generator.</param>
+        /// <param name="interfaceCodeGenerator">The interface code generator.</param>
+        /// <param name="classCodeGenerator">The class code generator.</param>
+        /// <param name="methodCodeGenerator">The method code generator.</param>
+        public WrapperGenerator(
+            INamespaceCodeGenerator namespaceCodeGenerator,
+            IAttributeCodeGenerator attributeCodeGenerator,
+            IInterfaceCodeGenerator interfaceCodeGenerator,
+            IClassCodeGenerator classCodeGenerator,
+            IMethodCodeGenerator methodCodeGenerator)
+        {
+            this.namespaceCodeGenerator = namespaceCodeGenerator;
+            this.attributeCodeGenerator = attributeCodeGenerator;
+            this.interfaceCodeGenerator = interfaceCodeGenerator;
+            this.classCodeGenerator = classCodeGenerator;
+            this.methodCodeGenerator = methodCodeGenerator;
+        }
+
+        /// <summary>
+        /// Generates a Roslyn namespace definition from a type and its methods.
+        /// </summary>
+        /// <param name="options">The program options.</param>
+        /// <param name="typeNamespace">The namespace which the type belongs to.</param>
+        /// <param name="typeName">The name of the type.</param>
+        /// <param name="methods">The methods to be wrapped.</param>
+        /// <returns>The generated namespace declaration.</returns>
+        public NamespaceDeclarationSyntax Generate(
+            IWrapOptions options,
+            string typeNamespace,
+            string typeName,
+            IEnumerable<MethodInfo> methods)
+        {
+            Ensure.That(options).IsNotNull();
+            Ensure.That(typeNamespace).IsNotNullOrWhiteSpace();
+            Ensure.That(typeName).IsNotNullOrWhiteSpace();
+            Ensure.That(methods).IsNotNull();
+
+            // Add a prefix to create an interface name from the type name
+            var interfaceName = $"{Interface.DefaultPrefix}{typeName}";
+
+            var attributes = this.CreateAttributes(
+                options,
+                interfaceName);
+
+            var modifiers = new[]
+            {
+                SyntaxFactory.Token(SyntaxKind.PublicKeyword),
+                SyntaxFactory.Token(SyntaxKind.PartialKeyword)
+            };
+
+            var interfaceDeclaration = this.interfaceCodeGenerator.Generate(
+                interfaceName,
+                modifiers);
+
+            var classDeclaration = this.classCodeGenerator.Generate(
+                typeName,
+                attributes,
+                modifiers,
+                interfaceName);
+
+            // Run generation for each method
+            foreach (var method in methods)
+            {
+                var (methodDeclaration, methodBody) = this.methodCodeGenerator.Generate(method);
+
+                // Add method declaration to interface declaration
+                var interfaceMethodDeclaration = methodDeclaration
+                    .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken));
+
+                interfaceDeclaration = interfaceDeclaration.AddMembers(interfaceMethodDeclaration);
+
+                // Add method declaration and body to class definition
+                var classMethodDeclaration = methodDeclaration
+                    .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                    .WithBody(methodBody);
+
+                classDeclaration = classDeclaration.AddMembers(classMethodDeclaration);
+            }
+
+            var namespaceDeclaration = this.namespaceCodeGenerator.Generate(typeNamespace);
+
+            // Add the interface to the namespace
+            namespaceDeclaration = namespaceDeclaration.AddMembers(interfaceDeclaration);
+
+            // Add the class to the namespace
+            namespaceDeclaration = namespaceDeclaration.AddMembers(classDeclaration);
+
+            return namespaceDeclaration;
+        }
+
+        private AttributeSyntax[] CreateAttributes(
+            IWrapOptions options,
+            string interfaceName)
+        {
+            var attributes = new List<AttributeSyntax>
+            {
+                this.attributeCodeGenerator.Generate(typeof(DebuggerNonUserCodeAttribute)),
+                this.attributeCodeGenerator.Generate(typeof(ExcludeFromCodeCoverageAttribute)),
+                this.attributeCodeGenerator.Generate(typeof(GeneratedCodeAttribute))
+            };
+
+            var mefExportArgument = SyntaxFactory.AttributeArgument(
+                SyntaxFactory.TypeOfExpression(
+                    SyntaxFactory.IdentifierName(interfaceName)));
+
+            if (options.IncludeMefAttribute)
+            {
+                attributes.Add(this.attributeCodeGenerator.Generate(Attribute.MefExport, mefExportArgument));
+            }
+
+            if (options.IncludeMef2Attribute)
+            {
+                attributes.Add(this.attributeCodeGenerator.Generate(Attribute.Mef2Export, mefExportArgument));
+            }
+
+            return attributes.ToArray();
+        }
+    }
+}
